f8aca58a3d31edf0e073fa20a9b486f5dc22a93f
mharsh8788 <mharshwardhan2@gmail.com>
2025-07-03 10:48:37 +0530

    Create Batteryinvertor.sql

---
+++ Batteryinvertor.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS batteries (
+    id VARCHAR(20) PRIMARY KEY,
+    type VARCHAR(50),
+    battery_condition VARCHAR(20),
+    in_use BOOLEAN,
+    usage_count INTbatteriesbatteries
+);
